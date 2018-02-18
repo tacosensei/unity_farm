ad47c87bbee457da78191a2da6d92e8fa70bcfa4
alex <32718250+tacosensei@users.noreply.github.com>
2018-02-18 16:51:35 -0500

    Plot Adjacency

--- Assets/Scripts/CropPlot/definePlot.cs
+++ Assets/Scripts/CropPlot/definePlot.cs
@@ -6,6 +6,7 @@ public class definePlot : MonoBehaviour {
 	
 	public GameObject selection;
 	public GameObject plot;
+	public bool adjacentMode;
 	private GameObject pointer;
 	
 	void Start () {
@@ -17,11 +18,16 @@ public class definePlot : MonoBehaviour {
 		RaycastHit hit;
         if(Physics.Raycast(ray, out hit)) {
 			if (hit.transform.gameObject.tag == "valid") {
-				pointer.SetActive(true);
-				pointer.transform.position = hit.point;
+				if(hit.transform.gameObject.name == "Adjacent") {
+					pointer.transform.position = hit.transform.gameObject.transform.position;
+				}
+				else {
+					pointer.SetActive(true);
+					pointer.transform.position = hit.point;
+				}
 				if(Input.GetMouseButtonDown(0)) {
-					GameObject newPlot = Instantiate(plot);
-					newPlot.transform.position = pointer.transform.position;
+						GameObject newPlot = Instantiate(plot);
+						newPlot.transform.position = pointer.transform.position;
 				}
 			}
 			else {
