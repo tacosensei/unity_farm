ec81f1465bc1996d6957c0fff17035bf516aee97
alex <32718250+tacosensei@users.noreply.github.com>
2018-02-17 23:42:03 -0500

    Crop Script

---
+++ Assets/Scripts/CropPlot/definePlot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class definePlot : MonoBehaviour {
+	
+	public GameObject sphere;
+	
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray)) {
+                Instantiate(sphere, transform.position, transform.rotation);
+			}
+        }
+	}
+}
